0e6e6698badd03c6a6081add9f4523f6997b08bc
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-13 00:02:38 +0200

    Add project files.

---
+++ 13. IF Statement_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.IF_Statement_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter fist number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetMax(num1, num2));
+
+
+            //freeze console
+            Console.ReadLine();
+        }
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                Console.WriteLine("The max is ");
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine("The max is ");
+                return num2;
+            }
+        }
+    }
+}
